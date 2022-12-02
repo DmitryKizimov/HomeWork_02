726fb09a1f7478c97782cf587f598ec74ec8ca30
Dmitry Kizimov <kizimov@mail.ru>
2022-12-03 00:49:21 +0300

    Task 01 is working correct

---
+++ Task_01/Program.cs
@@ -0,0 +1,20 @@
+﻿//Запрашиваем трехзначное число
+Console.Write("Введите трехзначное число: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+int firstdigit = digit / 100;
+int temp = digit / 10;
+int seconddigit = temp % 10;
+int thirddigit = digit % 10;
+if (firstdigit > 10)
+{
+    Console.WriteLine("Я же просил трехзначное число!"); // Проверка, что не более 3-х знаков
+    return;
+}
+
+if (firstdigit == 0) // Проверка, что не менее 2-х знаков
+{
+    Console.WriteLine("Я же просил трехзначное число!");
+    return;
+}
+
+Console.WriteLine(seconddigit);
