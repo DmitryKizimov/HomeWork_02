146e54a7df132862153eb1ce8aec50a1d9435a85
Dmitry Kizimov <kizimov@mail.ru>
2022-12-03 01:55:46 +0300

    Task 03 exellent

---
+++ Task_03/Program.cs
@@ -0,0 +1,17 @@
+﻿//Запрашиваем произвольное число
+Console.Write("Введите число, проверим кратно ли оно, одновременно 7 и 23: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+
+int n1 = digit % 7;
+int n2 = digit % 23;
+
+//Console.WriteLine(n1);  // для отладки выводил значения, для работы программы эти строки не нужны
+//Console.WriteLine(n2);
+
+if (n1 == 0 && n2 == 0)
+{
+    Console.WriteLine("Да");
+    return;
+}
+
+Console.WriteLine("Нет");
